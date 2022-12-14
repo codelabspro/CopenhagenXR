d4b8fd883680f0deb5d62000ca9badc9869d8163
Arunabh Das <arunabhdas@gmail.com>
2022-12-14 07:06:13 -0500

    Initialized project

---
+++ CopenhagenXR/Assets/Scripts/ObstacleMoverControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMoverControl : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Store user input as a movement vector
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        body.MovePosition(this.transform.position + Vector3.left * Time.fixedDeltaTime * speed);
+    }
+}
---
+++ CopenhagenXR/Assets/Scripts/ObstacleSpawnController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawnController : MonoBehaviour
+{
+    public float spawnRange = 5;
+    public float spawnLocation = 10;
+    public GameObject obstacle;
+
+    [Header]("timer")]
+    public float spawnTime = 2;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = spawnTime;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = spawnTime;
+            var height = Random.Range(-spawnRange, spawnRange);
+            GameObject.Instantiate(obstacle, new Vector3(spawnLocation, height, 0), Quaternion.Identity);
+        }        
+    }
+}
